b5326e5fc3104f7888647e21222f43fd6cc56f09
maurogarro73 <maurogarro73@gmail.com>
2022-08-20 18:45:15 -0300

    agregando .find

--- js/index.js
+++ js/index.js
@@ -1,103 +1,93 @@
-alert('Bienvenidos a pampaTicket!');
+alert("Bienvenidos a pampaTicket!");
 
-class Ticket{
-    constructor(id, nombre, precio, stock){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
+class Ticket {
+  constructor(id, nombre, precio, stock) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+  }
 }
 
-const ticket1 = new Ticket(1, 'Babasonicos', 2900, 300);
-const ticket2 = new Ticket(2, 'El mato', 5000, 100);
-const ticket3 = new Ticket(3, 'Tren Loco', 1500, 400);
-const ticket4 = new Ticket(4, 'Celia Cruz', 1000, 200);
+const ticket1 = new Ticket(1, "Babasonicos", 2900, 300);
+const ticket2 = new Ticket(2, "El mato", 5000, 100);
+const ticket3 = new Ticket(3, "Tren Loco", 1500, 400);
+const ticket4 = new Ticket(4, "Celia Cruz", 1000, 200);
 
 const entradas = [];
 entradas.push(ticket1,ticket2,ticket3,ticket4);
 
+let opcionEntrada = Number(
+    prompt(
+      `1) Babasonicos - $2900
+      2) El Mato - $5000
+      3) Tren Loco - $1500
+      4) Celia Cruz - $1000
 
-let opcionEntrada = Number(prompt(
+      0) Para finalizar`
+    )
+);
+const ticketComprado = [];
+
+while(opcionEntrada != 0){
+  if (opcionEntrada) {
+      comprar(opcionEntrada);
+  }
+
+  opcionEntrada = Number(prompt(
     `1) Babasonicos - $2900
     2) El Mato - $5000
     3) Tren Loco - $1500
     4) Celia Cruz - $1000
 
     0) Para finalizar`
-));
+  ));
 
-const ticketComprado = [];
+  while(opcionEntrada === 0){
+    finalizarCompra()
+    alert('Gracias por su compra');
+    break;
+  }
 
-while(opcionEntrada != 0){
-    switch(opcionEntrada){
-        case 1:
-        comprar(opcionEntrada);
-        break;
-        case 2:
-        comprar(opcionEntrada);
-        break;
-        case 3:
-        comprar(opcionEntrada);
-        break;
-        case 4:
-        comprar(opcionEntrada);
-        break;
-        default:
-        alert('Opción incorrecta');
-        break;
-    }
-
-    opcionEntrada = Number(prompt(
-        `1) Babasonicos - $2900
-        2) El Mato - $5000
-        3) Tren Loco - $1500
-        4) Celia Cruz - $1000
-
-        0) Para finalizar`
-    ));
-
-    while(opcionEntrada === 0){
-        finalizarCompra()
-        alert('Gracias por su compra');
-        break;
-    }
 }
 
-function comprar(opcionEntrada){
-    id = opcionEntrada - 1;
-    alert(`Usted ha seleccionado entradas de ${entradas[id].nombre}`);
+function comprar(opcionEntrada) {
+  const selectedTicket = entradas.find(
+    (entrada) => entrada.id === opcionEntrada
+  );
+  alert(`Usted ha seleccionado entradas de ${selectedTicket.nombre}`); //Luego uso selectedTicket para mostrar los datos
 
-    this.stock = Number(prompt(`cantidad de entradas para ${entradas[id].nombre}`));
-    if(this.stock > entradas[id].stock){
-        alert(`No hay stock suficientes, solo quedan ${entradas[id].stock} entradas`);
-    }else {
-        entradas[id].stock = entradas[id].stock - this.stock;
-        ticketComprado.push(new Ticket(entradas[id].id, entradas[id].nombre, entradas[id].precio * stock, stock));
-    }
+  let cantEntradas = Number(prompt(`cantidad de entradas para ${selectedTicket.nombre}`));
+  if(cantEntradas > selectedTicket.stock){
+    alert(`No hay stock suficientes, solo quedan ${selectedTicket.stock} entradas`);
+  }else {
+    let id = opcionEntrada - 1;
+    entradas[id].stock = entradas[id].stock - cantEntradas;
+    ticketComprado.push(new Ticket(selectedTicket.id, selectedTicket.nombre, selectedTicket.precio * cantEntradas, cantEntradas));
+  }
 }
 
 function finalizarCompra(){
-    let cont = 0;
-    for(let i = 0; i < ticketComprado.length; i++){
-        alert(`Compro entradas de ${ticketComprado[i].nombre}
-                Cantidad: ${ticketComprado[i].stock}
-                Precio Total: $${ticketComprado[i].precio}`);
-        cont += ticketComprado[i].precio;
-    }
-    alert(`Total a pagar por todas las entradas es de $${cont}`);
-    console.log(ticketComprado);
+  let cont = 0;
+  for(let i = 0; i < ticketComprado.length; i++){
+      alert(`Compro entradas de ${ticketComprado[i].nombre}
+              Cantidad: ${ticketComprado[i].stock}
+              Precio Total: $${ticketComprado[i].precio}`);
+      cont += ticketComprado[i].precio;
+  }
+  alert(`Total a pagar por todas las entradas es de $${cont}`);
+  console.log(ticketComprado);
 }
 
 /* Ordenar el array de menor precio a mayor */
 const ticketOrdenados =  entradas.sort((a, b) => {
-                            if (a.precio > b.precio) {
-                                return 1;
-                            }
-                            if (a.precio < b.precio) {
-                                return -1;
-                            }
-                            return 0;
-                        })
+  if (a.precio > b.precio) {
+      return 1;
+  }
+  if (a.precio < b.precio) {
+      return -1;
+  }
+  return 0;
+})
 
 console.log(ticketOrdenados);
